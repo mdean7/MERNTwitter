7567b9f2fb7e2dc5c783b2e63966a8311c39775e
Mike <michaeldeanmdmph@gmail.com>
2021-04-18 13:26:13 -0700

    add next commit

---
+++ frontend/src/reducers/root_reducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import session from './session_reducer.js';
+import errors from './errors_reducer'
+const RootReducer = combineReducers({
+  session,
+  errors
+});
+
+export default RootReducer;
