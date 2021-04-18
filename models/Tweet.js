17f8d9feb0180f13956830955eca6a38d46b19ee
Mike <michaeldeanmdmph@gmail.com>
2021-04-17 22:21:11 -0700

    add next commit

--- models/Tweet.js
+++ models/Tweet.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+// users might need to be 'User' the way we exported it in the User model.
 const TweetSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
