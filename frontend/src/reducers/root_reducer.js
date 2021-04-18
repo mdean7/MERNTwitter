80d17a02a8b358da89319aa680277b5faccab2ac
Mike <michaeldeanmdmph@gmail.com>
2021-04-18 15:05:46 -0700

    add next commit

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer.js';
 import errors from './errors_reducer'
+import tweets from './tweet_reducer'
 const RootReducer = combineReducers({
   session,
-  errors
+  errors,
+  tweets
 });
 
 export default RootReducer;
