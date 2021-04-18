f14f00de6b0f229c0046d624c4b9e69246d6d538
Mike <michaeldeanmdmph@gmail.com>
2021-04-17 22:04:25 -0700

    add next commit

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
-const users = require("./routes/api/users");
-const tweets = require("./routes/api/tweets");
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require("./models/User")
+const passport = require('passport');
+
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true  })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Wut?"));
-app.use("/api/users", users);
-app.use("/api/tweets", tweets);
+app.get("/", (req, res) =>{ 
+// const user = new User({
+//   handle:"mike",
+//   email:"mike@mike.com",
+//   password: "123456"
+//   })
+//   user.save()
+  res.send("Wut?")
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
